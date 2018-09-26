880b7873d2fc32f2011cd2335251d37a14bc149a
Jostein Dyrseth <josdyr@gmail.com>
2018-09-26 14:54:24 +0100

    lab01_task01

---
+++ labs/lab01/lab01_task01_w_threads.cpp
@@ -0,0 +1,57 @@
+#include <thread>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <limits>
+#include <chrono>
+#include <tuple>
+#include <future>
+
+using namespace std ;
+using namespace std::chrono ;
+
+std::tuple<int, int> find_min_max(vector<int> random_numbers)
+{
+    int max = 0; //lowest nr
+    int min = std::numeric_limits<int>::max(); //inf. high nr
+
+    for (int r : random_numbers)
+    {
+        if (max < r)
+        {
+            max = r;
+        }
+        if (min > r)
+        {
+            min = r;
+        }
+    }
+
+    std::cout << max << ", " << min << "\n";
+
+    return std::make_tuple(min, max);
+}
+
+int main(int argc, char **argv)
+{
+    vector<int> random_numbers;
+    random_device r;
+    default_random_engine e(r());
+
+    auto start = system_clock::now();
+
+    //generate random nr and append to vector
+    for (int i = 0; i < 10000000; ++i)
+    {
+        random_numbers.push_back(e());
+    }
+
+    //create a thread and execute the find_min_max
+    auto future = async(find_min_max, random_numbers);
+
+    auto end = system_clock::now();
+    auto total = end - start;
+    std::cout << total.count() << "ms" << "\n";
+
+    return 0;
+}
